6f2b25c7fdcecb1cc63de52a5ab4dcc595b596cd
Wilson Chen <wilsonchen901@gmail.com>
2023-03-28 14:24:56 -0400

    complete Binary Search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+    let lo = 0;
+    let hi = haystack.length;
+
+    do {
+        const m = Math.floor(lo + (hi - lo) / 2);
+        const val = haystack[m];
+
+        if (val === needle) {
+            return true;
+        } else if (val > needle) {
+            hi = m;
+        } else {
+            lo = m + 1;
+        }
+   } while (lo < hi);
+   return false;
 }
